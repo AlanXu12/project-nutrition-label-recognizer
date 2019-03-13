a7890ace0c3a4a34dffe7b028deaba210f475c05
hwj0418 <ddh0418@gmail.com>
2019-03-13 19:12:06 -0400

    updated search bar component

--- nuxpert/src/components/Search_bar.jsx
+++ nuxpert/src/components/Search_bar.jsx
@@ -1,32 +1,50 @@
 import React, { Component } from 'react';
+import './Search_bar.css'
 
-class SearchBar extends Component {
-
+class Search extends Component {
     state = {
-        keyword: 'default'
+        query: '',
+        results: {}
     }
 
+    // getResult = () => {
+    //     axios.get(`${API_URL}?api_key=${API_KEY}&prefix=${this.state.query}&limit=7`)
+    //         .then(({ data }) => {
+    //             this.setState({
+    //                 results: data.data
+    //             })
+    //         })
+    // }
+
     handleInputChange = () => {
-        console.log(this.state.query);
         this.setState({
             query: this.search.value
-        })
-        
-        
+        });
+        console.log(this.state.query);
+        // this.setState({
+        //     query: this.search.value
+        // }, () => {
+        //     if (this.state.query && this.state.query.length > 1) {
+        //         if (this.state.query.length % 2 === 0) {
+        //             this.getResult()
+        //         }
+        //     } else if (!this.state.query) {
+        //     }
+        // })
     }
 
     render() {
         return (
             <form>
                 <input
-                    className="search_bar"
                     placeholder="Search for..."
                     ref={input => this.search = input}
                     onChange={this.handleInputChange}
                 />
+                {/* <Suggestions results={this.state.results} /> */}
             </form>
-        );
+        )
     }
 }
 
-export default SearchBar;
+export default Search
--- nuxpert/src/pages/Result.jsx
+++ nuxpert/src/pages/Result.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import {BrowserRouter as Router, Route} from 'react-router-dom';
 import { Redirect } from 'react-router-dom'
 import './Result.css';
-import './styles.css';
+// import './styles.css';
 
 import sampleImg from '../media/sample-nutrition-label-ca.png';
 
