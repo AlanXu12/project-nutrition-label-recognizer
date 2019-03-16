e98562ffa6aaa988ac2dfa06e87d0c777dd5beae
hwj0418 <ddh0418@gmail.com>
2019-03-16 01:51:19 -0400

    testing img search

--- nuxpert/src/App.js
+++ nuxpert/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home.jsx'
+import Result from './pages/Result';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <Router>
         <div>
           <Route exact path="/" component = {Home}/>
+          <Route exact path="/result" component = {Result}/>
         </div>
       </Router>
     );
