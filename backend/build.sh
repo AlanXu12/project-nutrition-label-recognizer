fc3a08b716826ae24daf05cf9280dfb149c88783
Conner Shen <connershen98@hotmail.com>
2019-03-16 22:52:18 -0400

    Updated deploy strategy

--- backend/build.sh
+++ backend/build.sh
@@ -3,5 +3,6 @@ docker build -t node-app .
 docker run -p 8080:8080 -d --name node-app node-app
 sleep 5s
 docker logs node-app
-# docker run -p 8080:8080 -d --name node-app-1 node-app-1
-# docker logs node-app-1
+sleep 3s
+docker tag node-app gcr.io/kubernetes-221921/node-app:latest
+docker push gcr.io/kubernetes-221921/node-app
