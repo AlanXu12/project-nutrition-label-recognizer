fe1efa10a5ea520ec9fd4101aaca1987b7d3c0f5
Conner Shen <connershen98@hotmail.com>
2019-03-17 20:16:14 -0400

    updated everything to dynamically fetching data from whatever Home Page sent

--- nuxpert/src/pages/Result.jsx
+++ nuxpert/src/pages/Result.jsx
@@ -22,24 +22,51 @@ import CreditPortal from '../components/CreditPortal.jsx';
 
 class Result extends Component {
 
-  state = {
-    redirect: false,
-    title: "Default Title",
-    details: "Defualt details Defualt details Defualt details Defualt details",
-    x: 0,
-    y: 0,
-    nutriRangeArr: [
-      {name: "fat", yMin: 139, yMax: 156 },
-      {name: "sugars", yMin: 233, yMax: 249 },
-      {name: "sodium", yMin: 301, yMax: 319 },
-      {name: "protein", yMin: 252, yMax: 273 },
-      {name: "calories", yMin: 105, yMax: 126 }
-    ],
-    curNutri: "Default",
-    image: null,
-    imageHeight: 0,
-    imageWidth: 0
+  constructor(props) {
+    super(props);
+    // get all saved data
+    console.log(this.props);
+    const prevState = this.props.location.state;
+    console.log(prevState);
+    // this.setState({
+    //   nutriRangeArr: prevState.result,
+    //   image: prevState.image,
+    //   imageHeight: prevState.result.height,
+    //   imageWidth: prevState.result.width
+    // });
+    this.state = {
+      redirect: false,
+      title: "Default Title",
+      details: "Defualt details Defualt details Defualt details Defualt details",
+      x: 0,
+      y: 0,
+      nutriRangeArr: prevState.result,
+      curNutri: "Default",
+      image: URL.createObjectURL(prevState.image),
+      imageHeight: prevState.result.height,
+      imageWidth: prevState.result.width
+    };
+    console.log(this.state);
   }
+
+  // state = {
+  //   redirect: false,
+  //   title: "Default Title",
+  //   details: "Defualt details Defualt details Defualt details Defualt details",
+  //   x: 0,
+  //   y: 0,
+  //   nutriRangeArr: [
+  //     {name: "fat", yMin: 139, yMax: 156 },
+  //     {name: "sugars", yMin: 233, yMax: 249 },
+  //     {name: "sodium", yMin: 301, yMax: 319 },
+  //     {name: "protein", yMin: 252, yMax: 273 },
+  //     {name: "calories", yMin: 105, yMax: 126 }
+  //   ],
+  //   curNutri: "Default",
+  //   image: null,
+  //   imageHeight: 0,
+  //   imageWidth: 0
+  // }
 
   // turn redirect flag to true
   setRedirect = () => {
@@ -83,28 +110,53 @@ class Result extends Component {
     console.log("zoomRatio: ", zoomRatio);
     console.log("nutriRangeArr: ", this.state.nutriRangeArr);
     // find the corresponding factor that the user clicked on
-    this.state.nutriRangeArr.map((nutri) => {
-      console.log("clicked X: ", this.state.x);
-      console.log("clicked Y: ", this.state.y);
-      console.log("relative Y: ", this.state.y / this.state.zoomRatio);
-      if ((typeof nutri) == "object") {
+    console.log(typeof nutriRangeArr);
+    const nutrients = this.state.nutriRangeArr;
+    Object.keys(nutrients).some((nutrient, index) => {
+      // console.log("clicked X: ", this.state.x);
+      // console.log("clicked Y: ", this.state.y);
+      // console.log("zoomRatio: ", zoomRatio);
+      // console.log("relative Y: ", this.state.y / zoomRatio);
+      if (nutrient != "height" && nutrient != "width") {
+        const nutri = nutrients[nutrient];
+        // console.log("nutrient: ", nutrient);
+        // console.log("nutri: ", nutri);
+        // console.log("nutri.yMin: ", nutri.yMin);
+        // console.log("nutri.yMax: ", nutri.yMax);
         if (nutri.yMin <= (this.state.y / zoomRatio) && (this.state.y / zoomRatio) <= nutri.yMax) {
+          // console.log("nutrient: ", nutrient);
           this.setState({
-            curNutri: nutri.name
+            curNutri: nutrient
           });
-          console.log("new curNutri: ", nutri.name);
-          this.getNutriDetails(nutri.name);
+          // console.log("new curNutri: ", nutrient);
+          this.getNutriDetails(nutrient);
           return;
         }
       }
     });
+    // forEach((nutri) => {
+    //   console.log("clicked X: ", this.state.x);
+    //   console.log("clicked Y: ", this.state.y);
+    //   console.log("relative Y: ", this.state.y / this.state.zoomRatio);
+    //   if ((typeof nutri) == "object") {
+    //     if (nutri.yMin <= (this.state.y / zoomRatio) && (this.state.y / zoomRatio) <= nutri.yMax) {
+    //       this.setState({
+    //         curNutri: nutri.name
+    //       });
+    //       console.log("new curNutri: ", nutri.name);
+    //       this.getNutriDetails(nutri.name);
+    //       return;
+    //     }
+    //   }
+    // });
+    
   }
 
   // set the size of current rendered result image
-  componentDidMount() {
-    const height = this.divElement.clientHeight;
-    this.setState({ height });
-  }
+  // componentDidMount() {
+  //   const height = this.divElement.clientHeight;
+  //   this.setState({ height });
+  // }
 
   // request backend for the current clicked factor's nutrition details and display the info
   getNutriDetails = async (nutriName) => {
@@ -123,17 +175,14 @@ class Result extends Component {
     console.log("this.state: ", this.state);
   };
 
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   return !equals(nextProps, this.props);
+  // }
+
 
   render() {
-    // get all saved data
-    const prevState = this.props.location.state;
-    console.log(prevState);
-    this.setState({
-      nutriRangeArr: prevState.result,
-      image: prevState.image,
-      imageHeight: prevState.result.height,
-      imageWidth: prevState.result.width
-    });
+
+    console.log("In render(): ", this.state);
 
     return (
       <div className="container">
