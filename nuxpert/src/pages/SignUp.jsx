ef84522a43f72c70b6e774ca7f573789f0a12452
Pingfan Xu <pingfan.xu@mail.utoronto.ca>
2019-03-30 20:21:03 -0400

    cleaned up SignUp page code

--- nuxpert/src/pages/SignUp.jsx
+++ nuxpert/src/pages/SignUp.jsx
@@ -7,33 +7,11 @@ import SignUpForm from '../components/SignUpForm.jsx';
 
 class SignUp extends Component {
 
-  state = {
-    redirect: false,
-    someData: [1, 2]
-  }
-  setRedirect = () => {
-    this.setState({
-      redirect: true
-    })
-  }
-  resultRedirect = () => {
-    if (this.state.redirect) {
-      return (
-        <Result someData={this.state.someData}/>
-      );
-    }
-  }
-
   render() {
-    console.log(this.state.someData);
     return (
       <div>
-        <NavBar {...this.props} />
-        {/*this.resultRedirect()*/}
-        {/*<button onClick={this.setRedirect}>To Result Page</button>*/}
-        <SignUpForm
-          {...this.props}
-        />
+        <NavBar { ...this.props } />
+        <SignUpForm { ...this.props }/>
       </div>
     );
   }
