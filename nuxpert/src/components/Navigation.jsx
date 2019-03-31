65a06a19987ce9dc45ed35bb0630c7290b2a08a4
hwj0418 <ddh0418@gmail.com>
2019-03-30 22:16:47 -0400

    nav bar
    clean up nav bar

--- nuxpert/src/components/Navigation.jsx
+++ nuxpert/src/components/Navigation.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import Cookies from 'js-cookie';
 import './Navigation.css'
-import SearchBar from './SearchBar'
 import {
     MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink,
     MDBNavbarToggler, MDBCollapse, MDBDropdown, MDBDropdownMenu,
-    MDBDropdownToggle, MDBDropdownItem
+    MDBDropdownToggle, MDBDropdownItem, MDBFormInline, MDBBtn
 } from 'mdbreact'
 
 class Navigation extends Component {
@@ -13,7 +12,9 @@ class Navigation extends Component {
         // inherit props from parent.
         super(props);
         this.state = {
-            collapseID: ''
+            collapseID: '',
+            keyword: '_',
+            fuzzy_result: {}
         }
     }
 
@@ -22,6 +23,39 @@ class Navigation extends Component {
         this.setState({ isOpen: !this.state.isOpen });
     }
 
+    handleFuzzySearch = async () => {
+        // send search to backend using fetch get
+        console.log("here?");
+        const response = await fetch('/api/fuzzy/nutrient/' + this.state.keyword + '/', {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+        });
+        // update state with response infomation
+        const body = await response.json();
+        if (response.status !== 200) throw Error(body.message);
+        if (body) {
+            this.setState({
+                keyword: this.state.keyword,
+                result: body
+            });
+            // and redirect to the search page.
+            const location = {
+                pathname: '/search/' + this.state.keyword,
+                state: this.state
+            }
+            this.props.history.push(location);
+            window.location.reload();
+        }
+    }
+
+    handleInputChange = () => {
+        this.setState({
+            keyword: this.search.value
+        });
+    }
 
     handleSignout = async () => {
         // send signout request to the backend and reset path to home page
@@ -90,8 +124,21 @@ class Navigation extends Component {
                         </div>
                     </MDBNavbarNav>
                     <MDBNavbarNav right>
-                        {/* search bar in nav bar */}
-                        <SearchBar {...this.props} />
+                        <MDBFormInline >
+                            <div className="md-form my-0">
+                                <input
+                                    className="form-control mr-sm-2"
+                                    type="text"
+                                    ref={input => this.search = input}
+                                    placeholder="Search Nutrient"
+                                    onChange={this.handleInputChange}
+                                />
+                            </div>
+                        </MDBFormInline>
+                        <button
+                            type="submit"
+                            onClick={this.handleFuzzySearch}
+                            className="fuzzySearchButton" >Search</button>
                     </MDBNavbarNav>
                 </MDBCollapse>
             </MDBNavbar>
