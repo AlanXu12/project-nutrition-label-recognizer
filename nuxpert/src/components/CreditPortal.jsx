d8c98ee9723f8c3ee9bce2484d35791897102cde
Pingfan Xu <pingfan.xu@mail.utoronto.ca>
2019-03-30 20:48:25 -0400

    cleaned up CreditPortal warnings

--- nuxpert/src/components/CreditPortal.jsx
+++ nuxpert/src/components/CreditPortal.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import { Redirect } from 'react-router-dom';
-import Home from '../pages/Home.jsx'
 import './CreditPortal.css';
 
 class CreditPortal extends Component {
